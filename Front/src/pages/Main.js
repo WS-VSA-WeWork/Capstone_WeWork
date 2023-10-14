dec755867cd6367a6b4a3616e8ad3ebebe0cccd1
AhnDo0 <i0796075@gmail.com>
2023-10-14 23:29:33 +0900

    [Chore] src폴더 생성

---
+++ Front/src/pages/Main.js
@@ -0,0 +1,21 @@
+import { View, Text, StatusBar, StyleSheet } from "react-native";
+
+const Main = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+
+export default Main;
