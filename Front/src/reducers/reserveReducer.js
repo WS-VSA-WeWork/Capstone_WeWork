e873abebc1a1f7f519fe92e1197fed88a317f7b1
AhnDo0 <i0796075@gmail.com>
2023-12-03 16:24:41 +0900

    [Feat] #22 firebase 예약 데이터 받아오는 함수 추가

--- Front/src/reducers/reserveReducer.js
+++ Front/src/reducers/reserveReducer.js
@@ -32,8 +32,36 @@ const reservationSlice = createSlice({
       .addCase(pushReservData.rejected, (state, action) => {
         state.status = "failed";
         state.error = action.error.message;
+      })
+      .addCase(fetchReservData.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchReservData.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.data = action.payload; // 불러온 데이터를 상태에 저장
+      })
+      .addCase(fetchReservData.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
       });
   },
 });
+
+// 예약 데이터를 불러오는 비동기 함수
+export const fetchReservData = createAsyncThunk(
+  "reservation/fetchReservData",
+  async (queryParam) => {
+    // queryParam은 검색 조건을 나타냅니다 (예: userId, pubName)
+    const querySnapshot = await getDocs(collection(db, "pubReservations"), where(...queryParam.pubName));
+    let reservData = [];
+    querySnapshot.forEach((doc) => {
+      reservData.push(doc.data());
+    });
+    return reservData;
+  }
+);
+
+
+
 
 export default reservationSlice.reducer;
