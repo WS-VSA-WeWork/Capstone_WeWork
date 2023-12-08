e5b081ac956a0cf85ff2cee2f0d2471a46c0eaf9
wandong <122766043+dongwan97@users.noreply.github.com>
2023-12-08 15:54:51 +0900

    Fix : 예약메인 css 버그 수정, 내예약에서 모달창 페이지로 변환, 예약 데이터 연동

--- Front/src/pages/MyReservation.js
+++ Front/src/pages/MyReservation.js
@@ -7,10 +7,12 @@ import {
   Button,
   Pressable,
   TouchableOpacity,
+  TextInput,
 } from "react-native";
 import { useEffect, useState } from "react";
 import { FontAwesome } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
+import { Feather } from "@expo/vector-icons";
 
 import { useDispatch, useSelector } from "react-redux";
 import { AntDesign } from "@expo/vector-icons";
@@ -21,12 +23,16 @@ import { fetchReservationDataByUserId } from "../reducers/reservationReducer";
 const MyReservation = () => {
   const [haveReservation, setHaveReservation] = useState(false);
   const [modalVisible, setModalVisible] = useState(false);
+  const [typing, setTyping] = useState(false);
+  const [keyboardHeight, setKeyboardHeight] = useState(0);
 
   const navigation = useNavigation();
+
   const dispatch = useDispatch();
   const reservationData = useSelector((state) => state.reservation.data);
   const status = useSelector((state) => state.reservation.status);
   const error = useSelector((state) => state.reservation.error);
+  const [value, onChangeText] = useState("환불 사유를 입력해주세요");
 
   useEffect(() => {
     const userid = "1234567890"; // 임시로 넣어놓은 uid
@@ -63,21 +69,23 @@ const MyReservation = () => {
           </Text>
 
           {/* 이용전 예약건 */}
-          <View style={styles.reservationListContainer}>
-            <View style={styles.reservationList1}>
-              <Text style={styles.reservationListTitle}>
-                {reservationData[0].pubName}{" "}
-              </Text>
+          <TouchableOpacity onPress={() => navigation.navigate("환불")}>
+            <View style={styles.reservationListContainer}>
+              <View style={styles.reservationList1}>
+                <Text style={styles.reservationListTitle}>
+                  {reservationData[0].pubName}{" "}
+                </Text>
+              </View>
+              <View style={styles.reservationList2}>
+                <Text>{reservationData[0].reservDate}</Text>
+                <Text>{reservationData[0].reserveTime}</Text>
+              </View>
+              <View style={styles.reservationList3}>
+                <Text>예약금</Text>
+                <Text>{reservationData[0].deposit}원</Text>
+              </View>
             </View>
-            <View style={styles.reservationList2}>
-              <Text>{reservationData[0].reservDate}</Text>
-              <Text>{reservationData[0].reserveTime}</Text>
-            </View>
-            <View style={styles.reservationList3}>
-              <Text>예약금</Text>
-              <Text>{reservationData[0].deposit}원</Text>
-            </View>
-          </View>
+          </TouchableOpacity>
 
           {/* 전체 대관 내역 */}
           <View style={styles.reservationPast}>
@@ -136,16 +144,78 @@ const MyReservation = () => {
                 예약을 진행해주세요 !
               </Text>
             </View>
-            <View style={styles.myreservationButton}>
-              <Button
-                title="지금 예약하러가기"
-                color={"black"}
-                onPress={() => setHaveReservation(true)}
-              />
-            </View>
           </View>
         </View>
       )}
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}
+      >
+        {/* <TouchableWithoutFeedback
+            onPress={() => setModalVisible(false)}
+            style={styles.backdrop}
+          /> */}
+
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <View style={styles.modalTitleContainer}>
+              <Text style={styles.modalTitle}>식당 정보</Text>
+              <Pressable onPress={() => setModalVisible(false)}>
+                <Feather name="x" size={25} color="black" />
+              </Pressable>
+            </View>
+            <View style={styles.modalRestaurantTop}>
+              <Text style={styles.modalRestaurantName}>
+                {reservationData[0].pubName}{" "}
+              </Text>
+              {/* <Text style={styles.modalRestaurantTime}>19시간 전</Text> */}
+            </View>
+            <View style={styles.modalRestaurantMiddle}>
+              <Text style={styles.modalRestaurantDate}>
+                {reservationData[0].reservDate}
+              </Text>
+              <Text style={styles.modalRestaurantTime}>
+                {reservationData[0].reserveTime}
+              </Text>
+            </View>
+            <View style={styles.modalRestaurantBottom}>
+              <Text style={styles.modalRestaurantMoney}>예약금</Text>
+              <Text style={styles.modalRestaurantMoney}>
+                {reservationData[0].deposit}원
+              </Text>
+            </View>
+            <View style={{}}>
+              <TextInput
+                style={styles.want}
+                onChangeText={(text) => onChangeText(text)}
+                value={value}
+                placeholder="환불사유를 입력해주세요"
+                multiline
+                numberOfLines={5}
+                maxLength={40}
+                onPressIn={() => setTyping(true)}
+              ></TextInput>
+            </View>
+            <View style={styles.wantButton}>
+              <Button
+                color={"white"}
+                onPress={(() => setTyping(false), () => setModalVisible(false))}
+                style={styles.wantButton}
+                title="환불 요청하기"
+              ></Button>
+            </View>
+            <View
+              style={{
+                height: typing ? 0 : keyboardHeight - 20,
+              }}
+            ></View>
+          </View>
+        </View>
+      </Modal>
     </ScrollView>
   );
 };
